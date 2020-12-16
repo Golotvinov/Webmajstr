e66b3a7ed91edf321df305a44d2afe510f9ffaaf
Aleks Golotvinov <golotvinov@gmail.com>
2020-12-16 17:20:07 +0100

    Modal, JS, design

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ include 'includes/header.php';
 
 <body>
 <section class="main">
-    <img src="img/webmajstrlogo.svg"  class="logo-big" alt="Logo stránky">
+    <img src="img/webmajstrlogo.svg" class="logo-big" alt="Logo stránky">
     <h1 class="hero">Vítá vás <span>Webmajstr</span>!</h1>
     <h3>Webmajstr je webová miniaplikace, která vám pomůže pochopit základy webového vývoje. Představí vám jak web
         funguje, fundamentální koncepty kódování, tipy a triky a nakonec vám nabídne i kvíz, kde si budete moci
@@ -14,13 +14,24 @@ include 'includes/header.php';
         <img src="img/webwireframe.svg" alt="Vektorové schéma prototypu webových stránek">
     </section>
     <div class="btn">
-        <button class="button2" onclick="location.href='#'" type="button">JDEME NA TO!</button>
+        <button id="button2" class="button" type="button">JDEME NA TO!</button>
+    </div>
+    <div id="myModal" class="modal">
+        <div class="modal-content">
+            <span class="close">&times;</span>
+            <h2>Předtím než začneme prosím uveď svoje jméno:</h2>
+            <br><br>
+            <form>
+                <label for="fname">Jméno</label>
+                <input type="text" id="fname" name="fname">
+            </form>
+            <br>
+            <button class="button" type="button">Hotovo!</button>
+        </div>
     </div>
 </section>
 
-<footer>Toto je seminární práce k předmětu </footer>
-
-<script src="/js/main.js"></script>
+<script src="js/main.js"></script>
 </body>
 
 </html>
