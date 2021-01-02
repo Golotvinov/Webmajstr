79e7c058c104756cb852ade2b4afe05cfa5203c5
Aleks Golotvinov <golotvinov@gmail.com>
2021-01-02 16:41:36 +0100

    Pridani obsahu na hlavni strance

--- index.php
+++ index.php
@@ -1,31 +1,36 @@
 <?php
 include 'includes/header.php';
 ?>
-<header>
-    <div class="header-logo"><img src="img/webmajstrlogo.svg" alt="Logo stránky"></div>
-    <nav class="navbar">
-        <div class="dropdown">
-            <button class="dropbtn">O Webu</button>
-            <div class="dropdown-content">
-                <a href="#">Internet</a>
-                <a href="#">Webové prohlížeče</a>
+
+    <header>
+        <div class="header-logo"><img src="img/webmajstrlogo.svg" alt="Logo stránky"></div>
+        <nav class="navbar">
+            <div class="dropdown">
+                <button class="dropbtn">O Webu</button>
+                <div class="dropdown-content">
+                    <a href="#">Internet</a>
+                    <a href="#">Webové prohlížeče</a>
+                </div>
             </div>
-        </div>
-        <div class="dropdown">
-            <button class="dropbtn">Grafika</button>
-            <div class="dropdown-content">
-                <a href="#">Rozvržení</a>
-                <a href="#">Barvy</a>
-                <a href="#">Typografie</a>
+            <div class="dropdown">
+                <button class="dropbtn">Grafika</button>
+                <div class="dropdown-content">
+                    <a href="#">Rozvržení</a>
+                    <a href="#">Barvy</a>
+                    <a href="#">Typografie</a>
+                </div>
             </div>
-        </div>
-        <a href="#news">HTML</a>
-        <a href="#news">CSS</a>
-        <a href="#news">Nástroje</a>
-        <a href="#news">Kvíz</a>
-    </nav>
-<p>Vítej, <span id="lsOutput"></span>!</p>
-</header>
+            <a href="#news">HTML</a>
+            <a href="#news">CSS</a>
+            <a href="#news">Nástroje</a>
+            <a href="#news">Kvíz</a>
+        </nav>
+        <p>Vítej, <span id="lsOutput"></span>!</p>
+    </header>
+    <section class="content">
+    <h2>Vyber si z hlavní nabídky téma, které tě zajímá.</h2>
+        <p class="description">Na Webmajstru si můžeš témata studovat v libovolném pořadí, ale doporučujeme se držer>
+    </section>
 <?php
 include 'includes/footer.php';
 ?>
