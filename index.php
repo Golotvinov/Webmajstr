32d1908d6649a7b4678c6db3c432819fe989c492
Aleks Golotvinov <golotvinov@gmail.com>
2021-01-02 14:08:45 +0100

    main nav

--- index.php
+++ index.php
@@ -1,9 +1,31 @@
 <?php
 include 'includes/header.php';
 ?>
-
-<p><span id="lsOutput"></span> bude brzy umět tvořit weby!</p>
-    <br><br><br><br><br><br>
+<header>
+    <div class="header-logo"><img src="img/webmajstrlogo.svg" alt="Logo stránky"></div>
+    <nav class="navbar">
+        <div class="dropdown">
+            <button class="dropbtn">O Webu</button>
+            <div class="dropdown-content">
+                <a href="#">Internet</a>
+                <a href="#">Webové prohlížeče</a>
+            </div>
+        </div>
+        <div class="dropdown">
+            <button class="dropbtn">Grafika</button>
+            <div class="dropdown-content">
+                <a href="#">Rozvržení</a>
+                <a href="#">Barvy</a>
+                <a href="#">Typografie</a>
+            </div>
+        </div>
+        <a href="#news">HTML</a>
+        <a href="#news">CSS</a>
+        <a href="#news">Nástroje</a>
+        <a href="#news">Kvíz</a>
+    </nav>
+<p><span id="lsOutput"></span> bude brzy mistrem webů!</p>
+</header>
 <?php
 include 'includes/footer.php';
 ?>
