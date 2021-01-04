cff92a481eb80e291f1c87c5b380061fe0403067
Aleks Golotvinov <golotvinov@gmail.com>
2021-01-04 10:52:43 +0100

    Dynamicke nacitani postranneho obsahu + nove stranky (test)

---
+++ includes/page/internet.php
@@ -0,0 +1,30 @@
+<section id="main-content-area">
+    <h2>Internet</h2>
+    <p class="description">Na Webmajstru si můžeš témata studovat v libovolném pořadí, ale doporučujeme se držet
+        logicky navrženého pořadí, pokud s webovým designem a vývojem nemáš žádné předchozí zkušenosti. Postupuj
+        v
+        hlavní nabídce zleva doprava, od sekce <a href="#">O Webu</a> až po <a href="#">Kvíz</a>, ve kterém si
+        budeš
+        moci procvičit své zanlosti.</p>
+    <p class="description">Tento projekt vznikl ve spolupráci s <a href="http://it.pedf.cuni.cz/" target="_blank">Katedrou informačních technologií a technické výchovy UK v Praze</a>.</p>
+    <hr class="content-divider">
+    <h3>Obash:</h3>
+    <h4>O Webu</h4>
+    <p class="description">Ideální místo začít. Zde se dozvíš co je internet a jak funguje a pak i něco málo o internetových prohlížečích, tedy o software-u, který slouží k jeho prohlížení.</p>
+    <a href="#" class="fatLink">Internet</a><a href="#" class="fatLink">Webové prohlížeče</a>
+    <h4>Grafika</h4>
+    <p class="description">V této sekci se naučíš základní principy tvorby webového designu (webové grafiky).</p>
+    <a href="#" class="fatLink">Rozvržení</a><a href="#" class="fatLink">Barvy</a><a href="#" class="fatLink">Typografie</a>
+    <h4><a href="#">HTML</a></h4>
+    <p class="description">HTML je kostrou každého webu a tudíž i základ pro webový vývoj. Chceš-li začít tvořit webové stránky, pak musíš tento značkovací jazyk perfektně ovládat.</p>
+    <h4><a href="#">CSS</a></h4>
+    <p class="description">Kaskádové styly nám pomáhají web „obarvit", přidat animace, a de facto přeměnit grafický návrh na funkční web.</p>
+    <H4><a href="#">Nástroje</a></H4>
+    <p class="description">Zde najdeš celou řadu užitečných nástrojů a aplikací, které jsme osobně vybrali, a které ti pomůžou s tvorbou webových stránek a aplikací. Ať už to bude grafika či vývoj.</p>
+    <h4><a href="#">Kvíz</a></h4>
+    <p class="description">Chceš si zkusit jak moc dobře ovládáš základy webového designu a vývoje? Máme pro tebe připravený malý test, pomocí kterého zjistíš jak na tom jsi.</p>
+    <hr class="content-divider">
+    <h3>O projektu</h3>
+    <p class="description">Tento web vytvořil Aleks D. Golotvinov jako seminární práci na předmět Tvroba a vývoj webových aplikací, ve spolupráce s KITTV a panem doktorem T. Jeřábkem.</p>
+</section>
+</section>
