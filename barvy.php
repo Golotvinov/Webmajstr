39bf49e6469af3dddb877715113a664a6512239a
Aleks Golotvinov <golotvinov@gmail.com>
2021-01-09 23:24:25 +0100

    stranka o barvach

--- barvy.php
+++ barvy.php
@@ -95,11 +95,40 @@ include 'includes/header.php';
         </aside>
         <section id="main-content-area">
             <h2>Barvy</h2>
-            <p class="text">Než začneme vyrábět webovou stránku (resp. něž začneme cokoliv kódovat či programovat) tak
-                je nutné mít představu o tom co bude obsahem, komu bude applikace sloužit a jak bude vypadat. Při
-                grafickém návrhu řešíme poslední složku. Máme-li jasné první dva body, můžeme začít s prvním krokem
-                designu - rozložením stránky (layout-em). Nebo-li co kde bude. Layout nám definuje kde se na stránce
-                bude nacházet každá jednotlivá sekce a každý ovládací prvek.</p>
+            <p class="text">Zkušený web designer nikdy nevybírá barvy a barevné schéma pro web náhodně. Různé barvy slouží různým účelům, vyvolávají v nás určité pocity a emoce (i když se nám to nemusí zdát), sdělují nepřímo uživatelům o čem web je, co reprezentuje. Je to jakýsi nepsaný, neverbální jazyk, který komunikuje nějaké základní myšlenky projektu.</p>
+            <hr class="content-divider">
+            <h4>Základy barevné teorire:</h4>
+            <p class="text">S barevnou teorií přišel v roce 1666 Isaac Newton, který udělal projekci barevného spektra do kruhu. S tímto barevným kruhem pracují grafici i v dnešní době, jelikož ukazuje na vztahy mezi jednotlivými barvami. Na webu pracujeme samozřejmě s RGB verzí barevného kruhu (existují i další typy jako např. RYB - red, yellow, blue; se kterou pracují převážně malíři).</p>
+            <h4>Symbolika základních barev</h4>
+            <p class="text">Zde je stručný seznam základních barev a jejich asociace. Při tvorbě barevné palety na web můžete vycházet z těchto principů, za které vděčíme hlavně psychologii. Při výběru dominantní barvy tedy hodně záleží na tom jaké bude hlavní téma webu a jak má působit na návštěvníka.</p>
+            <p class="text">
+            <ul class="no-margin-ul">
+                <li><b style="color: var(--red)">Červená</b> - důležitost, láska. Dobrá pro zvýraznění nejpodstatnějších prvků na stránce. Často u chybových hlášení.</li>
+                <li><b style="color: #ff4901">Oranžová</b> - energie, zábava. Často se používá v eshopech pro zvýraznění lepší ceny.</li>
+                <li><b style="color: #eec806">Žlutá</b> - štěstí, pozornost. Vhodné použít při nějakém upozornění.</li>
+                <li><b style="color: #00de0a">Zelená</b> - úspěch, příroda. Ideální pro správně zvolené objekty (např. správná odpověď) nebo jako potvrzení, že se něco povedlo.</li>
+                <li><b style="color: #0239ff">Modrá</b> - důvěra, komfort, progres. Evokuje pocit bezpečí. Často používají software-ové firmy (IBM, Intel, Facebook, ...).</li>
+                <li><b>Fialová</b> - luxus, kreativita. Luxusní zboží, loajalita.</li>
+                <li><b style="color: #000000">Černá</b> - síla, sofistikovanost. Často dominantní barva ve fashion webech. Symbol lusuxu, exkluzivity.</li>
+                <li><b style="color: #a4a4a4">Bílá</b> - čistota, zdraví, minimalismus. Používána obzvlášť často v medicíně, popisuje čistý, bezpečný produkt.</li>
+            </ul>
+            </p>
+            <h4>Kombinace barev</h4>
+            <p class="text">Nemusíme mít cit pro design, abychom správně navolili paletu. O vše se postará věda. Máme k dispozici pět základních barevných kombinačních systémů. Jen je potřeba brát v potaz, že <b>čím více barev - tím hůř se dělá balans</b>.</p>
+            <p class="text">
+            <ul class="no-margin-ul">
+                <li><b>Komplementární</b> - barvy na protějších / opačných koncích barevného kruhu. Vysoký kontrast, křiklavé, velmi silný dojem.</li>
+                <li><b>Analogové</b> - barvy, které jsou vedle sebe na kruhu. Doporučuje se používat s jinou, dominantní barvou.</li>
+                <li><b>Triadická (triadic)</b> - tři barvy stejně vzdálené od sebe. Podobný výsledej jako komplementární systém, ale je zde více možností.</li>
+                <li><b>Tetradická (tetradic)</b> - čtyři barvy stejně vzdálené od sebe. Nejlépe funguje zvolit jednu dominantní a 3 doplňující.</li>
+                <li><b>Monochromatická</b> - různé odstíny stejné barvy.</li>
+            </ul>
+            </p>
+            <img src="img/ColorWheel.svg" alt="Mapa WWW" class="contentImg">
+            <h4>Užitečné odkazy k barvám</h4>
+            <a href="https://color.adobe.com/cs/create/color-wheel" class="fatLink" target="_blank">Adobe Color</a>
+            <a href="https://colordrop.io/" class="fatLink" target="_blank">Color Drop</a>
+            <a href="https://colorhunt.co/" class="fatLink" target="_blank">Color Hunt</a>
         </section>
     </section>
 <?php
