891ca8092a65b394d1565972b83d407afc484303
Aleks Golotvinov <golotvinov@gmail.com>
2021-01-09 23:34:54 +0100

    opravy chyb + error stranka

--- includes/error.php
+++ includes/error.php
@@ -1,3 +1,5 @@
-<center>
+<div style="text-align: center">
 	<h1>Not found 404</h1>
-</center>
+	<h2>Dostal(a) jses do zákoutí temných a dalekých.</h2>
+    <h3><a href="https://webmajstr.golotvinov.com">Zpět do světa živých</a></h3>
+</div>
